185b48399769ad3d98f07c206f54147544b561ff
dhruv50ae <linkdp2012@gmail.com>
2022-04-17 21:42:17 +0100

    Used a few more tools

--- draw.py
+++ draw.py
@@ -1,28 +1,43 @@
 import cv2 as cv
+import numpy as np
 
 img = cv.imread("Photos/dog1.jpg")
 
 cv.imshow("Dog", img)
 
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-cv.imshow("Gray", gray)
 
-blur = cv.GaussianBlur(img, (7, 7), cv.BORDER_DEFAULT)
-cv.imshow("Blur", blur)
+def translate(img, x, y):
+    transMat = np.float32([[1, 0, x], [0, 1, y]])
+    dimensions = (img.shape[1], img.shape[0])
+    return cv.warpAffine(img, transMat, dimensions)
 
-canny = cv.Canny(img, 125, 175)
-cv.imshow("Canny", canny)
 
-dilate = cv.dilate(canny, (7, 7), iterations=3)
-cv.imshow("DIalated", dilate)
+translated = translate(img, 100, 100)
+cv.imshow("Translated", translated)
 
-erode = cv.erode(dilate, (3, 3), iterations=1)
-cv.imshow("Erode", erode)
 
-resized = cv.resize(img, (500, 500), interpolation=cv.INTER_AREA)
-cv.imshow("Resize", resized)
+def rotate(img, angle, rotPoint=None):
+    (height, width) = img.shape[:2]
 
-cropped = img[50:200, 200:400]
+    if rotPoint is None:
+        rotPoint = (width//2, height//2)
+
+    rotMat = cv.getRotationMatrix2D(rotPoint, angle, 1.0)
+    dimensions = (width, height)
+
+    return cv.warpAffine(img, rotMat, dimensions)
+
+
+rotated = rotate(img, 45)
+cv.imshow("Rotated", rotated)
+
+resized = cv.resize(img, (500, 500), interpolation=cv.INTER_CUBIC)
+cv.imshow("Resized", resized)
+
+flip = cv.flip(img, 1)
+cv.imshow("Flip", flip)
+
+cropped = img[200:300, 300:400]
 cv.imshow("Cropped", cropped)
 
 cv.waitKey(0)
