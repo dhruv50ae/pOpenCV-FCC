04e9c44925732296a70f28ae4c4bb59bc2883e33
dhruv50ae <linkdp2012@gmail.com>
2022-04-17 10:35:07 +0100

    Created a video scaler

--- read.py
+++ read.py
@@ -1,20 +1,28 @@
 import cv2 as cv
 
-# img = cv.imread('Photos/dog1.jpg')
+img = cv.imread("Photos/dog1.jpg")
+cv.imshow("Cat", img)
 
-# cv.imshow("Dog", img)
+
+def rescaleFrame(frame, scale=0.75):
+    width = int(frame.shape[1]*scale)
+    height = int(frame.shape[0]*scale)
+    dimentions = (width, height)
+    return cv.resize(frame, dimentions, interpolation=cv.INTER_AREA)
+
 
 capture = cv.VideoCapture('Videos/doge.mp4')
 
 while True:
     isTrue, frame = capture.read()
 
+    frameResized = rescaleFrame(frame, scale=0.5)
+
     cv.imshow("Video", frame)
+    cv.imshow("Video Resized", frameResized)
 
     if cv.waitKey(20) & 0xFF == ord('d'):
         break
 
 capture.release()
 cv.destroyAllWindows
-
-# cv.waitKey(0)
